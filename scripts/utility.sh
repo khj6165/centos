6a68d53f4051942736c5526507f2f0c85a35dbfd
hyojeong <khj6165@gmail.com>
2019-02-07 09:28:08 +0900

    add mediainfo

--- scripts/utility.sh
+++ scripts/utility.sh
@@ -9,6 +9,7 @@ yum -y install tigervnc
 yum -y install filezilla
 yum -y install tree
 yum -y install ImageMagick
+yum -y install mediainfo
  
 #ripgrep
 yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
