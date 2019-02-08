b72c7f0188891e8e52aed6fbf05466adae0643c8
khyojeong <khj6165@gmail.com>
2019-02-08 16:44:44 +0900

    add Cython

--- scripts/pip.sh
+++ scripts/pip.sh
@@ -1,8 +1,10 @@
-#!/bin/sh/
-
+#!/bin/sh
 cd /tmp
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 
-pip install --upgrade setuptools
-pip install pymediainfo
+#pip install --upgrade setuptools #err
+pip install --user pymediainfo
+pip install --user PySide2
+pip install --user numpy 
+pip install --user Cython
