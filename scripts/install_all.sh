a771a27d66ef2129c80d8cbeca26a8bebb493d16
khyojeong <khj6165@gmail.com>
2019-02-13 14:13:45 +0900

    add alembic

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -11,3 +11,9 @@
 ./ffmpeg.sh
 ./handbrake.sh
 ./ocio_config.sh
+./alembic.sh
+
+#gcc6.x를 이용해서 빌드한다.
+scl enable devtoolset-6 ./cmake.sh
+scl enable devtoolset-6 ./openexr.sh
+scl enable devtoolset-6 ./opencolorio.sh
